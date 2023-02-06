881f8ec5285d658518129d64215085d5795a8045
SaMariani <samuele.mariani.01@gmail.com>
2023-02-06 10:31:56 +0100

    implementation Site

--- Mariani/DataModel.cs
+++ Mariani/DataModel.cs
@@ -1,15 +1,212 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using TAP22_23.AlarmClock.Interface;
 using TAP22_23.AuctionSite.Interface;
 
-namespace Mariani;
-
-public class MarianiContext : TapDbContext
+namespace Mariani
 {
-    public MarianiContext(string connectionString) : base(new DbContextOptionsBuilder<MarianiContext>().UseSqlServer(connectionString).Options) { }
-    //public DbSet<User> Users { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class MarianiContext : TapDbContext
     {
-        base.OnModelCreating(modelBuilder);
+        public MarianiContext(string connectionString) : base(new DbContextOptionsBuilder<MarianiContext>().UseSqlServer(connectionString).Options) { }
+        //public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Auction> Auctions { get; set; }
+
+    }
+    [Index(nameof(Name), IsUnique = true, Name = "NameUnique")]
+
+    public class Site : ISite
+    {
+        public int SiteId { get; set; }
+        public string Name { get; set; }
+        //todo check estremi range
+        [Range(DomainConstraints.MinTimeZone, DomainConstraints.MaxTimeZone)]
+        public int Timezone { get; set; }
+        [Range(0, int.MaxValue)]
+        public int SessionExpirationInSeconds { get; set; }
+        [Range(0, int.MaxValue)]
+        public double MinimumBidIncrement { get; set; }
+        public List<User> Users { get; set; }
+        [NotMapped]
+        public IAlarmClock AlarmClock { get; set; }
+        [NotMapped]
+        public string ConnectionString { get; set; }
+        public Site(string name, int timezone, int sessionExpirationInSeconds, double minimumBidIncrement, IAlarmClock alarmClock, string connectionString)
+        {
+            Name = name;
+            Timezone = timezone;
+            SessionExpirationInSeconds = sessionExpirationInSeconds;
+            MinimumBidIncrement = minimumBidIncrement;
+            Users = new List<User>();
+            AlarmClock = alarmClock;
+            ConnectionString = connectionString;
+        }
+
+        public Site(string name, int timezone, int sessionExpirationInSeconds, double minimumBidIncrement)
+        {
+            Name = name;
+            Timezone = timezone;
+            SessionExpirationInSeconds = sessionExpirationInSeconds;
+            MinimumBidIncrement = minimumBidIncrement;
+            Users = new List<User>();
+        }
+
+        public IEnumerable<IUser> ToyGetUsers()
+        {
+            using (var db = new MarianiContext(ConnectionString))
+            {
+                try
+                {
+                    var i = db.Users.ToList();
+                    return i;
+                }//todo check exceptions
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e)
+                {
+                    //TODO verify that codes etc confirm my guess
+                    throw new AuctionSiteNameAlreadyInUseException("cannot insert");
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteConcurrentChangeException();
+                }
+            }
+        }
+
+        public IEnumerable<ISession> ToyGetSessions()
+        {
+            using (var db = new MarianiContext(ConnectionString))
+            {
+                try
+                {
+                    var i = db.Sessions.ToList();
+                    return i;
+                }//todo check exceptions
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e)
+                {
+                    //TODO verify that codes etc confirm my guess
+                    throw new AuctionSiteNameAlreadyInUseException("cannot insert");
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteConcurrentChangeException();
+                }
+            }
+        }
+
+        public IEnumerable<IAuction> ToyGetAuctions(bool onlyNotEnded)
+        {
+            using (var db = new MarianiContext(ConnectionString))
+            {
+                try
+                {
+                    var i = db.Auctions.ToList();
+                    return i;
+                }//todo check exceptions
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e)
+                {
+                    //TODO verify that codes etc confirm my guess
+                    throw new AuctionSiteNameAlreadyInUseException("cannot insert");
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteConcurrentChangeException();
+                }
+            }
+        }
+
+        public ISession? Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime Now()
+        {
+            if (AlarmClock == null) throw new AuctionSiteInvalidOperationException("error");
+            return AlarmClock.Now;
+        }
+    }
+    
+
+    public class User : IUser
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public User(string username)
+        {
+            Username = username;
+        }
+        public IEnumerable<IAuction> WonAuctions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class Auction : IAuction
+    {
+        public int AuctionId { get; set; }
+        public int Id { get; }
+        public IUser Seller { get; }
+        public string Description { get; }
+        public DateTime EndsOn { get; }
+        public IUser? CurrentWinner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CurrentPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Bid(ISession session, double offer)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- Mariani/Logic.cs
+++ Mariani/Logic.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using TAP22_23.AlarmClock.Interface;
 using TAP22_23.AuctionSite.Interface;
+using System;
 
 namespace Mariani
 {
 
     public class MarianiHostFactory : IHostFactory
     {
-        public Host? Host { get; set; }
         public void CreateHost(string connectionString)
         {
             if (String.IsNullOrEmpty(connectionString))
@@ -19,7 +21,6 @@ namespace Mariani
                 {
                     c.Database.EnsureDeleted();
                     c.Database.EnsureCreated();
-                    Host = new Host(connectionString);
                 }
                 catch (SqlException e)
                 {
@@ -36,32 +37,135 @@ namespace Mariani
         {
             if (String.IsNullOrEmpty(connectionString))
                 throw new AuctionSiteArgumentNullException("connection strings cannot be null or empty");
-            if (alarmClockFactory==null) 
+            if (alarmClockFactory == null)
                 throw new AuctionSiteArgumentNullException("AlarmClock cannot be null");
-            if (Host == null) throw new AuctionSiteUnavailableDbException("Db has not been initialized yet");
-            if (Host.ConnectionString!=connectionString)
-                throw new AuctionSiteUnavailableDbException("Unavailable Db");
-            return Host;
+            using (var c = new MarianiContext(connectionString))
+            {
+                try
+                {
+                    c.Database.EnsureCreated();
+                }
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unexpected error", e);
+                }
+            }
+            return new Host(connectionString, alarmClockFactory);
         }
     }
     public class Host : IHost
     {
         public string ConnectionString { get; set; }
-        public Host(string connectionString)
+        public IAlarmClockFactory AlarmClockFactory { get; set; }
+        public Host(string connectionString, IAlarmClockFactory alarmClockFactory)
         {
             ConnectionString = connectionString;
+            AlarmClockFactory = alarmClockFactory;
         }
         public IEnumerable<(string Name, int TimeZone)> GetSiteInfos()
         {
-            throw new NotImplementedException();
+            using (var db = new MarianiContext(ConnectionString))
+            {
+                try
+                {
+                    var i = db.Sites
+                        .Select(s =>
+                            new ValueTuple<string, int>(s.Name, s.Timezone)
+                        ).ToList();
+                    return i;
+                }//todo check exceptions
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e)
+                {
+                    //TODO verify that codes etc confirm my guess
+                    throw new AuctionSiteNameAlreadyInUseException("cannot insert");
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteConcurrentChangeException();
+                }
+            }
         }
 
         public void CreateSite(string name, int timezone, int sessionExpirationTimeInSeconds, double minimumBidIncrement)
         {
-            throw new NotImplementedException();
+            if (DomainConstraints.MinTimeZone > timezone || DomainConstraints.MaxTimeZone < timezone) throw new AuctionSiteArgumentOutOfRangeException("timezone must be between -12, 12");
+            if(string.Empty==name) throw new AuctionSiteArgumentException("names cannot be null or empty");
+            if (0 >= minimumBidIncrement) throw new AuctionSiteArgumentOutOfRangeException("minimum bid increment must be positive");
+            if (0 >= sessionExpirationTimeInSeconds) throw new AuctionSiteArgumentOutOfRangeException("session expiration time must be positive");
+            if(null==name) throw new AuctionSiteArgumentNullException("");
+            var site = new Site(name, timezone, sessionExpirationTimeInSeconds, minimumBidIncrement, AlarmClockFactory.InstantiateAlarmClock(timezone), ConnectionString);
+            using (var c = new MarianiContext(ConnectionString))
+            {
+                try
+                {
+                    c.Sites.Add(site);
+                    c.SaveChanges();
+                }
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e)
+                {
+                    //TODO verify codes
+                    throw new AuctionSiteNameAlreadyInUseException("cannot insert");
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteConcurrentChangeException();
+                }
+            }
         }
 
         public ISite LoadSite(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new AuctionSiteArgumentNullException("name cannot be null or empty");
+            using (var c = new MarianiContext(ConnectionString))
+            {
+                try
+                {
+                    var site = c.Sites.Single(u => u.Name == name);
+                    return new Site(site.Name,site.Timezone,site.SessionExpirationInSeconds,site.MinimumBidIncrement, AlarmClockFactory.InstantiateAlarmClock(site.Timezone), ConnectionString);
+                }
+                catch (SqlException e)
+                {
+                    throw new AuctionSiteUnavailableDbException("Unavailable Db", e);
+                }
+                catch (DbUpdateException e)
+                {
+                    //TODO verify codes
+                    throw new AuctionSiteNameAlreadyInUseException("cannot insert");
+                }
+                catch (Exception e)
+                {
+                    throw new AuctionSiteInexistentNameException("Unavailable Db");
+                }
+            }
+            
+        }
+    }
+
+    public class Session : ISession
+    {
+        public int SessionId { get; set; }
+        public string Id { get; }
+        public DateTime ValidUntil { get; }
+        public IUser User { get; }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAuction CreateAuction(string description, DateTime endsOn, double startingPrice)
         {
             throw new NotImplementedException();
         }
